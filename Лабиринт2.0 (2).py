976590a64f205b5bb0074da277f9f6f39cfdb24c
IvanShandybin <ivan.shandybin6@gmail.com>
2024-11-19 22:52:06 +0300

    Update Лабиринт2.0 (2).py
    Работа на расстановкой объектов

--- Лабиринт2.0 (2).py
+++ Лабиринт2.0 (2).py
@@ -1,5 +1,4 @@
-import pygame, sys
-
+import pygame,sys
 pygame.init()
 width=int(input())
 height=int(input())
@@ -8,6 +7,8 @@ a=(0,0,0)
 b=(255,255,255)
 disp=pygame.display.set_mode((width+800,height))
 Objekt=None
+zvet=0
+r=0
 font = pygame.font.Font(None, 36)
 Paravila="1-Минотавр 2-Ключ 3-Выход 4-Начало реки 5-Конец реки"
 Paravila2="6-Река 7-Стена 8-Портал 9-Больница 0-Начало"
@@ -22,28 +23,42 @@ while True:
         if c.type==pygame.QUIT:
             pygame.quit()
             sys.exit()
+        elif c.type == pygame.MOUSEBUTTONDOWN and c.button == 1:
+            r=1
+            xcor=c.pos[0]
+            ycor=c.pos[1]
 ###############################################################################################################
 ######## Проверка последней нажатой цифры #####################################################################
         if c.type==pygame.KEYDOWN:
             if c.key==pygame.K_1:
+                zvet=1
                 Objekt="Минотавр"
             elif c.key==pygame.K_2:
-                Objekt= "Ключ "
+                zvet=2
+                Objekt= "Ключ"
             elif c.key==pygame.K_3:
+                zvet=3
                 Objekt= "Выход"
             elif c.key==pygame.K_4:
+                zvet=4
                 Objekt="Начало Реки"
             elif c.key==pygame.K_5:
+                zvet=5
                 Objekt="Конец Реки"
             elif c.key==pygame.K_6:
+                zvet=6
                 Objekt="Река"
             elif c.key==pygame.K_7:
+                zvet=7
                 Objekt="Стена"
             elif c.key==pygame.K_8:
+                zvet=8
                 Objekt="Портал"
             elif c.key==pygame.K_9:
+                zvet=9
                 Objekt="Больница"
             elif c.key==pygame.K_0:
+                zvet=10
                 Objekt= "Начало"
 ###############################################################################################################
 ###############################################################################################################
@@ -51,6 +66,8 @@ while True:
     for row in range(height//size):
         for col in range(width//size):
             pygame.draw.rect(disp,a,(col*size,row*size,size,size),1)
+    if zvet != 0 and r!=0:
+            pygame.draw.rect(disp, (20*zvet, 20*zvet, 20*zvet), (xcor,ycor, size, size), 100)
     if Objekt is not None:
         text=font.render(Objekt,True,a)
         text_rect=text.get_rect(center=(width+100,height//2))
