84b0f0cef42893e585ea1bd5d91e2df68a4c05e0
IvanShandybin <ivan.shandybin6@gmail.com>
2025-03-09 16:49:53 +0300

    Update Для идей лабиринта.py

--- Для идей лабиринта.py
+++ Для идей лабиринта.py
@@ -25,7 +25,6 @@ def proverka(xn,yn):
         Objekt3="Начало"
     return Objekt3
 def run_infinite_field():
-<<<<<<< HEAD
     subprocess.run([sys.executable, 'd:\Загрузки\бесконечное поле.py'])
 def reca(xn,yn):
         for h in range(len(rekax)):
@@ -50,9 +49,6 @@ def portal(xn,yn):
         xn=portx[nom]
         yn=porty[nom]
         return xn,yn
-=======
-    subprocess.run([sys.executable, 'd:\Загрузки\бесконечное поле.txt'])
->>>>>>> 55ea5f7171cc46658a38a6cb48d511bfd5c17c3d
 width=int(input())
 height=int(input())
 size=100
